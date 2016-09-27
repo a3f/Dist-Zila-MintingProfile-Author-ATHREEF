aa593e6a5168e3d79c0c8bb88367a0b951880b42
Ahmad Fatoum <ahmad@a3f.at>
2016-09-27 18:22:27 +0200

    Initial commit

---
+++ lib/Dist/Zila/MintingProfile/Author/ATHREEF.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+package Dist::Zila::MintingProfile::Author::ATHREEF;
+
+# ABSTRACT: a3f's Dist::Zilla Minting Profile
+# VERSION
+
+use Moose;
+use namespace::autoclean;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+__END__
+
+=head1 NAME
+
+Dist::Zila::MintingProfile::Author::ATHREEF - a3f's Dist::Zilla Minting Profile
+
+=head1 GIT REPOSITORY
+
+L<http://github.com/a3f/Dist-Zila-MintingProfile-Author-ATHREEF>
+
+=head1 SEE ALSO
+
+L<Dist::Zilla|Dist::Zilla>
+L<http://blog.fox.geek.nz/2011/01/making-minting-profile-as-cpanized-dist.html>
+
+=head1 AUTHOR
+
+Ahmad Fatoum C<< <athreef@cpan.org> >>, L<http://a3f.at>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 Ahmad Fatoum
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
---
+++ share/profiles/default/Module.pm
@@ -0,0 +1,73 @@
+use strict;
+use warnings;
+package {{$name}};
+
+# ABSTRACT: 
+# VERSION
+
+use Carp;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+{{$name}} - 
+
+
+=head1 SYNOPSIS
+
+    use {{$name}};
+
+
+=head1 DESCRIPTION
+
+=head1 IMPLEMENTATION
+
+=head1 METHODS AND ARGUMENTS
+
+=over 4
+
+=item new()
+
+Constructs a new {{$name}} instance.
+
+=cut
+
+sub new {
+	my $class = shift;
+    
+    my $self = {};
+
+	bless $self, $class;
+	return $self;
+}
+
+
+
+1;
+__END__
+
+=back
+
+=head1 GIT REPOSITORY
+
+L<http://github.com/a3f/{{$dist->name}}>
+
+=head1 SEE ALSO
+
+L<Perl|Perl>
+
+=head1 AUTHOR
+
+Ahmad Fatoum C<< <athreef@cpan.org> >>, L<http://a3f.at>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 Ahmad Fatoum
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
